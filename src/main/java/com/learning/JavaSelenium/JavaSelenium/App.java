a43b49cc529242cba611fa7521ac84c08f881376
Maninderpal Singh <manindersinghdhaliwal01@gmail.com>
2023-07-10 17:57:55 -0400

    Initial commit

---
+++ src/main/java/com/learning/JavaSelenium/JavaSelenium/App.java
@@ -0,0 +1,32 @@
+package com.learning.JavaSelenium.JavaSelenium;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+        driver.get("https://tutorialsninja.com/demo/");
+        
+        driver.findElement(By.linkText("My Account")).click();
+        driver.findElement(By.linkText("Login")).click();
+        driver.findElement(By.id("input-email")).sendKeys("seleniumpanda2@gmail.com");
+        driver.findElement(By.id("input-password")).sendKeys("Selenium@123");
+        driver.findElement(By.cssSelector("input.btn.btn-primary")).click();
+		driver.findElement(By.linkText("Logout")).click();
+		driver.findElement(By.linkText("Continue")).click();
+		
+		driver.quit();
+    }
+}
